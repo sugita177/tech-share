1a117b577e01317f45e3221afc9dfbee6e722edd
sugita177 <moriaki.sugita.1730@gmail.com>
2026-02-06 16:31:37 +0900

    Larastanへの対応

--- app/Http/Requests/Article/CreateArticleRequest.php
+++ app/Http/Requests/Article/CreateArticleRequest.php
@@ -4,11 +4,22 @@ namespace App\Http\Requests\Article;
 
 use App\UseCases\Article\CreateArticleInput;
 use App\Domain\Enums\ArticleStatus;
+use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules\Enum;
+use Illuminate\Support\Facades\Auth;
 
 class CreateArticleRequest extends FormRequest
 {
+    /**
+     * ログインしている場合のみ許可する
+     */
+    public function authorize(): bool
+    {
+        // auth:sanctum ミドルウェアで認証を保証しているため、ここでは認可のみを扱う
+        return true;
+    }
+
     public function rules(): array
     {
         return [
@@ -21,15 +32,18 @@ class CreateArticleRequest extends FormRequest
 
     public function toInput(): CreateArticleInput
     {
-        // 文字列から Enum への変換。指定がなければ Published（または Draft）をデフォルトにする
+        /** @var User $user */
+        $user = $this->user();
+
+        // 文字列から Enum への変換。指定がなければ Publishedをデフォルトにする
         $status = ArticleStatus::tryFrom($this->validated('status')) ?? ArticleStatus::Published;
 
         return new CreateArticleInput(
-            userId: $this->user()?->id ?? 1, // 認証未実装なら一旦1
+            userId: $this->user()->id,
             title: $this->validated('title'),
             content: $this->validated('content'),
-            slug: $this->validated('slug'), // nullもあり得る
-            status: $status
+            status: $status,
+            slug: $this->validated('slug') // nullもあり得る
         );
     }
 }
